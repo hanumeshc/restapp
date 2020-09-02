f0be45e4096aa134cddc54b9aba027772f546e38
phanikumar <phanikumarp@users.noreply.github.com>
2020-09-02 14:44:05 +0530

    default case check

--- pkg_scripts/deb/postInstall.sh
+++ pkg_scripts/deb/postInstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ln -s /opt/restapp/restapp-0.1.0.jar /etc/init.d/restapp
-##/etc/init.d/restapp start
+sudo /etc/init.d/restapp start
 update-rc.d  -f restapp defaults
-sudo /etc/init.d/restapp stop
+#sudo /etc/init.d/restapp stop
 
 ###====> for Newrelic monitor
 #sudo mkdir -p /opt/newrelic/
