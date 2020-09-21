b19146abb87ad59c45815e86c66ff5b0f7becc20
opsmxdemo <60219075+opsmxdemo@users.noreply.github.com>
2020-09-21 11:08:30 +0530

    Update GreetingControllerTests.java

--- src/test/java/hello/GreetingControllerTests.java
+++ src/test/java/hello/GreetingControllerTests.java
@@ -16,7 +16,6 @@
 //package hello;
 //
 //
-//
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 //import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 //import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
