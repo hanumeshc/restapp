5c6f22ace3c184bdcbea11ee7b0a0a45dcfcae20
opsmxdemo <60219075+opsmxdemo@users.noreply.github.com>
2020-10-16 16:32:19 +0530

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -2,7 +2,7 @@
 ##====> for simple restapp
 (cd /opt;nohup java  -Dserver.port=8080 -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
 sudo chmod 775 /var/log/rest_service.log
-sudo tail -f /var/log/rest_service.log &
+sudo tail -f /var/log/rest_service.log 
 
 ##=====> for promethues
 #(cd /opt;nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
@@ -19,4 +19,4 @@ sudo tail -f /var/log/rest_service.log &
 #nohup stress-ng --vm 4 &
 #sed -i "s/# hostname: mymachine.mydomain/$hostname/g" /etc/dd-agent/datadog.conf
 #service datadog-agent start
-sleep infinity
+#sleep infinity
